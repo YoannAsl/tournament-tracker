4b37ffd55f714c643d1b43ccc0fc629f094e1daa
YoannAsl <47599801+YoannAsl@users.noreply.github.com>
2021-09-03 20:35:47 +0200

    Initial commit

---
+++ model.py
@@ -0,0 +1,10 @@
+class Tournament:
+    def __init__(self, name, place, date, turns, rounds, players, time, description):
+        self.name = name
+        self.place = place
+        self.date = date
+        self.turns = turns
+        self.rounds = rounds
+        self.players = players
+        self.time = time
+        self.description = description
