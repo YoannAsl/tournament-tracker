c153757db5241aae2efa04527a309e72b7d7fc71
YoannAsl <47599801+YoannAsl@users.noreply.github.com>
2021-09-13 23:01:38 +0200

    Added player and match classes

--- model.py
+++ model.py
@@ -1,10 +1,29 @@
+from faker import Faker
+
+fake = Faker()
+
+
 class Tournament:
-    def __init__(self, name, place, date, turns, rounds, players, time, description):
+    def __init__(self, name, place, date, rounds, players, time, description, turns=4):
         self.name = name
         self.place = place
         self.date = date
-        self.turns = turns
         self.rounds = rounds
         self.players = players
         self.time = time
         self.description = description
+        self.turns = turns
+
+
+class Player:
+    def __init__(self, last_name, first_name, birthdate, gender, ranking):
+        self.last_name = last_name
+        self.first_name = first_name
+        self.birthdate = birthdate
+        self.gender = gender
+        self.ranking = ranking
+
+
+class Match:
+    def __init__(self, player1, player2, player1_res, player2_res):
+        pass
